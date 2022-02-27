6f6bb4d1aac17c5c95d2557444a074619422bf5c
Zhiling <ling4github@gmail.com>
2022-02-27 16:01:07 -0600

    Add files via upload

---
+++ RShinyapp/leaflet_demo/ui.R
@@ -0,0 +1,15 @@
+library(shiny)
+library(dplyr)
+library(leaflet)
+library(IDDA)
+shinyUI(
+  # Use a fluid layout
+  fluidPage(
+    # Give the page a title
+    titlePanel(
+      HTML(
+        paste("Top 10 counties with the largest cumulative", '<br/>',
+              "infected count on December 31, 2020")
+      )),
+    mainPanel(leafletOutput("map"))
+  ))
---
+++ RShinyapp/state_cum_inf/ui.R
@@ -0,0 +1,19 @@
+library(shiny)
+library(dplyr)
+library(IDDA)
+library(plotly)
+
+fluidPage(
+  sliderInput("date.update",
+              label = h5("Select date"),
+              min = as.Date("2020-12-01"),
+              max = as.Date("2020-12-31"),
+              value = as.Date("2020-12-31"),
+              timeFormat = "%d %b",
+              animate = animationOptions(interval = 2000, loop = FALSE)
+  ),
+  # Show a plot of the generated distribution
+  mainPanel(
+    plotlyOutput("state_cum_inf", height = "100%", width = "150%")
+  )
+)
